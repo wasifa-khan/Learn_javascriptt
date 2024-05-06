fd247fbbab721087dcae3705ec02a7a7dea3d77b
= <>
2024-05-06 16:38:20 +0500

    update

---
+++ JavascriptClass01/script.js
@@ -0,0 +1,94 @@
+console.log("Hello World");
+console.log("Wasifa Khan");
+document.write("Learn Javascript");
+
+alert("Message is display on screen")
+
+var  firstname = "Wasifa";
+
+var firstname = "khan";
+console.log(firstname);
+var dateOfBirth; //declare
+dateOfBirth = "17-Dec-2002"; //initialize
+console.log(dateOfBirth);
+
+var hello;
+console.log(hello); //Undefined
+
+var lastname = "Wasifa";
+var lastname = "khan";
+console.log(lastname);
+
+let firstName = "Eshal";
+let firstName = "Irfan";
+
+
+
+
+
+
+
+var text = "It is invented in 1845";
+console.log(text);
+
+var name = "Wasifa Khan";
+var email = "wasifaasadk@gmail.com";
+var dateOfBirth = 2002;
+console.log(name + " " + email + " " + dateOfBirth);
+alert(name + "  " + email + "  " + dateOfBirth);
+console.log(email);
+console.log(dateOfBirth);
+
+
+let x = 11;
+let y = 3;
+// let result = x * y;
+// let result = x + y;
+console.log("The Addition of two number is: " + (x + y));
+console.log("The Muiltiplication of two number is: " + (x * y));
+console.log("The Subtraction of two number is: " + (x - y));
+console.log("The Division of two number is: " + (x / y));
+console.log("The modulus of two number is: " + (x % y));
+
+
+ var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum);
+
+
+Legal Variable
+let FirstName;
+let lastName;
+let lastname;
+let _dateOfBirth;
+let $email;
+let Age;
+let age21;
+let &hello;
+let /question;
+
+Illegal Variable
+let 12;
+let 12return;
+let !hello;
+let @34question;
+let "wasfa";
+let ?asddd;
+
+
+
+let rose = "Flower";
+console.log(Rose);
+
+
+console.log("myVar");
+console.log("hello");
+
+
+let costOfTicket = 600;
+let buyingTicket = 5;
+let result = costOfTicket * buyingTicket;
+console.log("Total Cost of Ticket: " + result);
+
+
