13a4decbaa3341d75d3d72a48331709f8efd7b9f
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-08 15:52:48 +0500

    Program02

---
+++ JavascriptClass02/script.js
@@ -0,0 +1,46 @@
+// num++ ;
+// num = num + 1;
+
+// let a = 3;
+// let b = a--;  //pre increment 
+// console.log(b);
+
+// let num = 1;
+// let newNum = ++num;
+// console.log(newNum);
+
+// let num = 12;
+// let newNum = ++num; //12+1 = 13
+// console.log(num);
+// console.log(newNum);
+
+
+// let totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+// let totalCost = 1 + (3 * 4); //13
+
+let resultOfComputation = (2 * 4) * 4 + 2;//34
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+
+let UserName = "Wasifa";
+
+// console.log(firstName + " " + lastName);
+
+// alert("Thanks, " + "         "+ UserName + "!");
+
+let x = 6;
+let y = 2;
+console.log(x  +  y);
+
+
+
+
+
+
+
+
+// ++a // pre increment
+// --a // pre decrement
+// a++ // post increment
+// a--// post decrement
