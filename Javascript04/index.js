f54a919e92b30854b6fceeccbdb21a51872bcca5
= <umrabadar555@gmail.com>
2024-05-15 16:12:20 +0500

    run

---
+++ Javascript04/index.js
@@ -0,0 +1,71 @@
+// let raining = false;
+// if(raining){
+//     console.log("Remember to bring an umbrella.");
+// }
+// else{
+//     console.log("Enjoy the sunny weather!");
+// }
+
+// let student_grade = +prompt("Enter your score");
+// if(student_grade >= 60){
+//     console.log("You passed the test!");
+// }
+// else{
+//     console.log("You failed the test.");
+// }
+
+// let number = +prompt("Enter the Number");
+// if(number > 0){
+//     console.log("The number is positive.");
+// }
+// else if(number < 0){
+//     console.log("The number is negative.");
+// }
+// else if(number == 0){
+//     console.log("The number is zero");
+// }
+// else{
+//    console.log("all number are integer"); 
+// }
+
+
+// let score = +prompt("Enter your score");
+// if(score >= 80 || score >= 60){   
+//     console.log("You passed");
+// } else {
+//     console.log("You failed");
+// }
+// let isSunny = true;
+// let isWarm = true ;
+
+// if (isSunny && isWarm) {
+//   console.log("Perfect weather for outdoor activities!");
+// } else {
+//   console.log("Maybe another day.");
+// }
+
+// let isRaining = false;
+
+// if (!isRaining) {
+//   console.log("It's not raining. Enjoy the day!");
+// } else {
+//   console.log("Don't forget your umbrella!");
+// }
+
+
+// let number = +prompt("Enter a number");
+
+// if (number % 2 === 0) {
+//     console.log("The number is even");
+// } else {
+//     console.log("The number is odd");
+// }
+
+let number1 = +prompt("Enter a number");
+
+if (number1 % 2 !== 0) {
+    console.log("The number is even");
+} else {
+    console.log("The number is odd");
+}
+
