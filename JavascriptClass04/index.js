5e6f32deb588024b0402f8bcc7591125241153a3
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-22 16:05:21 +0500

    Commit

--- JavascriptClass04/index.js
+++ JavascriptClass04/index.js
@@ -1,125 +1,140 @@
-// let weight = +prompt("Enter  Weight");
-// let time = +prompt("Enter Time");
-// let age = +prompt("Enter Age");
-// let gender = prompt("Enter Gender");
-// if (weight > 300 || time < 6) {  //weight = 244 && time= 4
-//      alert("Come to our tryout!");
-//      }
-//     else {
-//      alert("Come to our cookout!");
-//      }
+// // let weight = +prompt("Enter  Weight");
+// // let time = +prompt("Enter Time");
+// // let age = +prompt("Enter Age");
+// // let gender = prompt("Enter Gender");
+// // if (weight > 300 || time < 6) {  //weight = 244 && time= 4
+// //      alert("Come to our tryout!");
+// //      }
+// //     else {
+// //      alert("Come to our cookout!");
+// //      }
        
 
-// let weight = +prompt("Enter Weight");
-// let time = +prompt("Enter Time");
+// // let weight = +prompt("Enter Weight");
+// // let time = +prompt("Enter Time");
 
-// if (!(weight > 300 && time < 6)) {
+// // if (!(weight > 300 && time < 6)) {
 
-//       //!(444 >300 || 4 < 6)
-//     alert("Come to our tryout");
-// } else {
-//     alert("Come to our cookout!");
-// }
+// //       //!(444 >300 || 4 < 6)
+// //     alert("Come to our tryout");
+// // } else {
+// //     alert("Come to our cookout!");
+// // }
 
-    //  if (weight > 300 || time < 6 || age > 17 || gender === "male") {
-    //     // 78 > 300 ||  5 < 6  ||   16 > 17 || male === "male"
-    //     alert("Come to our tryout!");
-    //     }
-    //     else {
-    //     alert("Come to our cookout!");
-    //     }
+//     //  if (weight > 300 || time < 6 || age > 17 || gender === "male") {
+//     //     // 78 > 300 ||  5 < 6  ||   16 > 17 || male === "male"
+//     //     alert("Come to our tryout!");
+//     //     }
+//     //     else {
+//     //     alert("Come to our cookout!");
+//     //     }
      
     
-// let weight = +prompt("Enter Weight");
-// let time = +prompt("Enter Time");
+// // let weight = +prompt("Enter Weight");
+// // let time = +prompt("Enter Time");
 
 
-// if (weight > 300) {
-//     alert("Come to our side!");
-// } else {
-//     if (time < 6) {
-//         alert("Come to our tryout!");
-//     } else {
-//         alert("Come to our cookout!");
-//     }
-// }
+// // if (weight > 300) {
+// //     alert("Come to our side!");
+// // } else {
+// //     if (time < 6) {
+// //         alert("Come to our tryout!");
+// //     } else {
+// //         alert("Come to our cookout!");
+// //     }
+// // }
 
 
-// let x = 4;
-// let y = 4;
-// let a = 16;
-// let b = 20;
-// let c= 34;
-// let d = 36;
+// // let x = 4;
+// // let y = 4;
+// // let a = 16;
+// // let b = 20;
+// // let c= 34;
+// // let d = 36;
 
-// if ((x === y || a === b) && c === d) {
+// // if ((x === y || a === b) && c === d) {
     
-//     console.log("Correct");
-// } else {
+// //     console.log("Correct");
+// // } else {
    
-//     console.log("Incorrect");
-// }
+// //     console.log("Incorrect");
+// // }
 
-// let x = 4;
-// let y = 10;
+// // let x = 4;
+// // let y = 10;
 
-// if (x > 5) {
-//     if(y > 2) {
-//         console.log("Both x and y are greater than their respective thresholds.");
-//     } else {
-//         console.log("x is greater than 5, but y is not greater than 2.");
-//     }
-// } else {
-//     console.log("x is not greater than 5, so the inner condition is not checked.");
-// }
-
-
-let temperature = 15;
-let isSunny = true;
-
-if(temperature > 25){
-  console.log("It's hot outside!");
-
-  if(isSunny){
-      console.log("Don't forget to wear sunscreen.");
-  }
-  else{
-    console.log("You might still want to wear sunscreen.");
-  }
-}
+// // if (x > 5) {
+// //     if(y > 2) {
+// //         console.log("Both x and y are greater than their respective thresholds.");
+// //     } else {
+// //         console.log("x is greater than 5, but y is not greater than 2.");
+// //     }
+// // } else {
+// //     console.log("x is not greater than 5, so the inner condition is not checked.");
+// // }
 
 
+// let temperature = 15;
+// let isSunny = true;
 
+// if(temperature > 25){
+//   console.log("It's hot outside!");
 
-// else if(temperature >= 15 && temperature <= 25){
-//     console.log("It's a pleasant day.");
 //   if(isSunny){
-//     console.log("Enjoy the sunshine!");
+//       console.log("Don't forget to wear sunscreen.");
 //   }
 //   else{
-//     console.log("Even if it's cloudy, it's still nice outside.");
+//     console.log("You might still want to wear sunscreen.");
+//   }
 // }
-// }
+
+
+
+
+// // else if(temperature >= 15 && temperature <= 25){
+// //     console.log("It's a pleasant day.");
+// //   if(isSunny){
+// //     console.log("Enjoy the sunshine!");
+// //   }
+// //   else{
+// //     console.log("Even if it's cloudy, it's still nice outside.");
+// // }
+// // }
+// // else{
+// //     console.log("It's a bit chilly.");
+// //     if (isSunny) {
+// //         console.log("Don't be fooled by the sun, it's still cool.");
+// //     } else {
+// //         console.log("It might be cloudy, so bring a jacket just in case.");
+// //     }
+// // }
+
+// let age = 18;
+// let weight = 78;
+//  if(age > 20){
+//     if(weight > 45){
+// console.log("Your are in range");
+// } 
 // else{
-//     console.log("It's a bit chilly.");
-//     if (isSunny) {
-//         console.log("Don't be fooled by the sun, it's still cool.");
-//     } else {
-//         console.log("It might be cloudy, so bring a jacket just in case.");
-//     }
+//     console.log("You are out of range");
+
+// }
+// }
+// else { 
+//         console.log("Have a Good weight");
 // }
 
-let age = 18;
-let weight = 78;
- if(age > 20){
-    if(weight > 45){
-console.log("Your are in range");
-} 
-else{
-    console.log("You are out of range");
+//Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
 
+let gender = prompt("Enter your Gender");
+if(gender === "male"){
+  console.log("Good morning sir");
 }
+else if(gender === "female"){
+  console.log("Good morning maam");
 }
-else { 
-        console.log("Have a Good weight");
+else{
+  console.log("Other");
 }
---
+++ JavascriptClass05/index.js
@@ -0,0 +1,42 @@
+//Arrays
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+// console.log("Welcome to " + city3 );
+
+// let cities = ["Atlanta","Baltimore","Chicago","Denver","Los Angeles","Seattle"];
+// console.log(cities[0].length);
+// console.log(cities[1].length);
+// let number = [1, 4, 3, 7, 8, 9, 11, 43, 67,];
+// console.log(number[6].length);//undefined 
+
+// let info = ["Wasfa", 21, "Eshal", 10, "Raviya", 22]
+// console.log(info[0].length);
+// console.log(info[1].length);//undefined
+
+// let mixedArray = ["13", "Bob", "Now is", true];
+// console.log(mixedArray[0].length);
+// console.log(mixedArray[1].length);
+// console.log(mixedArray[4].length);
+
+
+//Arrays Method()
+// let cities = ["Atlanta","Baltimore","Chicago","Denver","Los Angeles","Seattle"];
+// // cities.push("Pakistan", "India");
+// // console.log(cities);
+// // cities.pop("India")
+// // console.log(cities);
+// // cities.pop("Pakistan");
+// // console.log(cities);
+// cities.toString();
+// console.log(cities);
+
+
+let cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+cities.push(12, 22)
+console.log(cities);
+console.log(cities.toString());
