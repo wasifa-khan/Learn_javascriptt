1659016c24bd7ca62adde389006cbade85fc5bea
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-29 16:04:17 +0500

    upadted

--- JavascriptClass06/index.js
+++ JavascriptClass06/index.js
@@ -19,7 +19,7 @@
 // fruits.shift();
 // console.log(fruits);
 
-//Slice(Return a piece of Array)
+// Slice(Return a piece of Array)
 // let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 // console.log(fruits);
 // let piece_of_Array = fruits.slice(1,-4)
@@ -28,9 +28,36 @@
 
 //Splice()
 
-let months = ["January", "February", "Monday", "Tuesday", "Saturaday"];
-let days = months.splice(2,0, "March");
-console.log(days);
-console.log(months);
+// let months = ["January", "February", "Monday", "Tuesday", "Saturaday"];
+// let days = months.splice(2,0, "March");
+// console.log(days);
+// console.log(months);
 
 // splice(start, remove, new )
+
+//Slice
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+// Slicing from index 1 to the end
+let piece1 = fruits.slice(1)
+console.log(piece1); // Output: ["Orange", "Lemon", "Apple", "Mango"]
+
+// Slicing from index -2 (second last) to the end
+let piece2 = fruits.slice(-2);
+console.log(piece2); // Output: ["Apple", "Mango"]
+
+// Slicing from index 1 to index 3 (exclusive)
+let piece3 = fruits.slice(1, 3);
+console.log(piece3); // Output: ["Orange", "Lemon"]
+
+// Slicing the entire array
+let piece4 = fruits.slice();
+console.log(piece4); // Output: ["Banana", "Orange", "Lemon", "Apple", "Mango"]
+
+// Slicing with negative indices
+let piece5 = fruits.slice(-3, -1);
+console.log(piece5); // Output: ["Lemon", "Apple"]
+
+// Slicing with no parameters (same as slice())
+let piece6 = fruits.slice(0, fruits.length);
+console.log(piece6); // Output: ["Banana", "Orange", "Lemon", "Apple", "Mango"]
---
+++ JavascriptClass07/index.js
@@ -0,0 +1,31 @@
+// const studentName = ["Micheal", "John", "Tony"];
+// const studentScore = [320, 230, 480];
+// let total = 500;
+// const percentage1 = (studentScore[0] / total) * 100;
+// const percentage2 = (studentScore[1] / total) * 100;
+// const percentage3 = (studentScore[2] / total) * 100;
+// document.write("Score of  " + studentName[0] + " is " + studentScore[0] + " . Percentage: " + percentage1 + "%");
+// document.write("<br> Score of  " + studentName[1] + " is " + studentScore[1] + " . Percentage: " + percentage2 + "%");
+// document.write("<br> Score of  " + studentName[2] + " is " + studentScore[2] + " . Percentage: " + percentage3 + "%");
+
+
+
+//For Loops
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+// for(let i = 0;i < 20;i++){
+//     console.log("Hello World");
+// }
+
+for (let i = 50; i > 4; i = i - 5) {
+    console.log(i);
+}
