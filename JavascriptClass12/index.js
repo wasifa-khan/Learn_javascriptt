38b3746f83d1117dde4fef3678bda9d6559f0f13
= <>
2024-06-24 15:56:29 +0500

    commit

---
+++ JavascriptClass12/index.js
@@ -0,0 +1,64 @@
+// let cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toUpperCase();
+// let cleanestCities = ["cheyenne", "sante fe", "tucson", "great falls", "honolulu"];
+// for (let i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+// alert("It's one of the cleanest cities");
+// }
+// }ot 
+
+
+
+// let country = "Pakistan";
+// console.log(country.indexOf('t'));
+
+// let country = "Pakistan";
+// console.log(country.lastIndexOf('a'));
+
+// let text = "To be or not to be.";
+// let segIndex = text.lastIndexOf("to be");
+// console.log(segIndex);
+
+
+// let firstName = prompt("Enter your First Name");
+// // let firstChar = firstName.slice(0, 1);
+// var firstChar = firstName.charAt(0)
+// console.log(firstChar);
+
+// let text = "HELLO WORLD";
+// let letter = text.charAt(text.length-4);
+// console.log(letter);
+
+// let text = "Hello! This is an example text!";
+// for (let i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//     alert("Exclamation point found!");
+//     break;
+//     }
+//     }
+
+
+// let text = "Visit Microsoft!";
+// let result = text.replace("Microsoft", 567);
+// console.log(result);
+
+// let text = "Mr Blue has a blue house and a blue car";
+// let result = text.replace(/blue/gi, "red");
+// console.log(result);
+
+
+//Rounding Number
+// let scoreAvg = Math.round(8.0678437);
+// console.log(scoreAvg);
+
+// var ceilNum = Math.ceil(8.5267821);
+// console.log(ceilNum);
+
+// var ceilNum = Math.floor(6.5267821);
+// console.log(ceilNum);
+
+//Generating Random Number
+let number = Math.floor(Math.random() * 10) + 1;
+console.log(number);
+
+
