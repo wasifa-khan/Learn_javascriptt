a6ec8ee433afbd0fcfdf76a09fa85c14497e14dc
= <>
2024-07-01 16:06:03 +0500

    commit

---
+++ JavascriptClass15/index.js
@@ -0,0 +1,12 @@
+//Changing elements of a date and time
+
+// let d = new Date();
+// // d.setFullYear(2008);
+// // d.setMonth(5);
+// // d.setDate(12)
+// d.setHours(4);
+// d.setMinutes(43)
+// d.setMilliseconds(324);
+
+// // console.log(d);
+// console.log(d);
---
+++ JavascriptClass16/index.js
@@ -0,0 +1,94 @@
+// [1,2,34].push(3);
+// console.log("hello");
+
+// function myFunction(){
+//     console.log("Hello World");
+//     console.log("I love Javascript");
+// }
+// myFunction();
+// myFunction();
+// myFunction();
+// myFunction();
+
+
+// function myFunction(a,b){ //parameter
+//     console.log(a + b);
+// }
+// myFunction(4,7);
+// myFunction(12, 45);
+// myFunction(12,90)//Argument
+
+// function sum(x, y){ 
+//     //local variable //scope
+//     s = x + y;
+//     console.log("before");
+//     return s;
+//     console.log("After");
+    
+// }
+// console.log(sum(23,56));
+
+
+// function tellTime() {
+//     var now = new Date();
+//     var theDay = now.getDay();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     alert("Current time: "+ theHr + ":" + theMin + ":" + theDay );
+//     }
+// tellTime()
+
+// function multilpication(p1, p2){
+//     return p1 * p2
+// }
+// console.log(multilpication(5, 8))
+
+// Convert Fahrenheit to Celsius:
+
+// function fahrenheitToCelsius(fahrenheit){
+//     return (5/9) * (fahrenheit-32);
+// }
+// console.log(fahrenheitToCelsius());
+
+
+// function square(a){
+//     return a * a;
+// }
+// console.log(square(6));
+// console.log(square(13));
+
+
+// function factorial(n) {
+//     if (n === 0 || n === 1) {
+//       return 1;
+//     } else {
+//       return n * factorial(n - 1);
+//     }
+//   }
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
+// console.log(factorial(6));
+// console.log(factorial(7));
+// console.log(factorial(8));
+// console.log(factorial(9));
+// console.log(factorial(10));
+
+
+// const additionOfNum = (a, b) =>{
+//     return a + b;
+// }
+// console.log(additionOfNum(56,87));
+
+const hello = () =>{
+   return "I love Javasript Coding";
+}
+console.log(hello());
+
+
+    
+
+
+
